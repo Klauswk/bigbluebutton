421514b6137d9c5e8269b7503fda286a557e95c7
Klaus Klein <klauswk1@hotmail.com>
2017-05-18 10:21:18 -0300

    Add test to check Acl

---
+++ bigbluebutton-html5/imports/api/acl/Acl.js
@@ -0,0 +1,93 @@
+import { check } from 'meteor/check';
+
+/**
+ * Class that represent the System Acl, both for server and client
+ * 
+ * The write option overlap the read
+ * @example (read = true, write = false) ask for read, result true
+ * @example (read = false, write = false) ask for read, result false
+ * @example (read = false, write = true) ask for read, result true
+ */
+export class Acl {
+
+  /**
+   * @param {object} aclConfig
+   * @param {object} Users 
+   */
+  constructor(aclConfig, Users) {
+    check(aclConfig, Object);
+
+    this.Users = Users;
+    this.aclConfig = aclConfig;
+  }
+
+  /**
+   * @param {string} permissionName - The name of the permission 
+   * @param {string} actionName - Read/Write
+   * @param {object} credentials - The users creedentials
+   * @return An boolean which inform if the action is allowed.
+   */
+  isAllowedTo(permissionName, actionName, credentials) {
+    check(credentials, Object);
+    check(actionName, String);
+    check(permissionName, String);
+
+    const meetingId = credentials.meetingId;
+    const userId = credentials.requesterUserId;
+    const authToken = credentials.requesterToken;
+
+    const user = this.Users.findOne({
+      meetingId,
+      userId,
+    });
+
+    if (!user.role) {
+      return false;
+    }
+
+    let role = this.roleExist(this.aclConfig, user.role);
+
+    if (role) {
+      return this.canPerformAction(permissionName, actionName, role);
+    }
+    return false;
+  }
+
+  /**
+   * Check if the user have the permission to execute the action
+   * @param {string} permissionName 
+   * @param {string} actionName 
+   * @param {object} role 
+   * @return {Boolean} return the action permission.
+   */
+  canPerformAction(permissionName, actionName, role) {
+    check(role, Object);
+    check(actionName, String);
+    check(permissionName, String);
+
+    const permission = role[permissionName];
+
+    if (permission) {
+      let action = permission[actionName];
+
+      //check if the action exist
+      if (action !== undefined) {
+        return (!!action || permission.write);
+      }
+      return false;
+    }
+    return false;
+  }
+
+  /**
+   * Check if the role exist.
+   * @param {object} acl 
+   * @param {string} userRole
+   * @return {object} the role object, or undefined if not found.  
+   */
+  roleExist(acl, userRole) {
+    check(acl, Object);
+    check(userRole, String);
+    return acl[userRole];
+  }
+}
---
+++ bigbluebutton-html5/imports/api/acl/test/acl.test.js
@@ -0,0 +1,106 @@
+const expect = require('expect.js');
+import { Acl } from '../Acl';
+
+describe('Acl Tests', function () {
+
+  describe('Viewer Tests', function () {
+    let acl;
+    beforeEach(function () {
+      acl = new Acl(aclConfig, UsersViewer);
+    });
+
+    it('Expect return false for inexistent permission', function () {
+      expect(acl.isAllowedTo("foo", "write", mockCredentials)).to.be(false);
+    });
+
+    it('Expect return false for inexistent action', function () {
+      expect(acl.isAllowedTo("chatPublic", "foo", mockCredentials)).to.be(false);
+    });
+
+    it('Expect return true for chatPublic', function () {
+      expect(acl.isAllowedTo("chatPublic", "write", mockCredentials)).to.be(true);
+    });
+
+    it('Expect return true for presentation read and false for write', function () {
+      expect(acl.isAllowedTo("presentation", "read", mockCredentials)).to.be(true);
+      expect(acl.isAllowedTo("presentation", "write", mockCredentials)).to.be(false);
+    });
+  });
+
+  describe('Moderator Tests', function () {
+    let acl;
+    beforeEach(function () {
+      acl = new Acl(aclConfig, UsersModerator);
+    });
+
+    it('Expect return false for inexistent permission', function () {
+      expect(acl.isAllowedTo("foo", "write", mockCredentials)).to.be(false);
+    });
+
+    it('Expect return false for inexistent action', function () {
+      expect(acl.isAllowedTo("chatPublic", "foo", mockCredentials)).to.be(false);
+    });
+
+    it('Expect return true for chatPublic', function () {
+      expect(acl.isAllowedTo("chatPublic", "write", mockCredentials)).to.be(true);
+    });
+
+    it('Expect return true for presentation read and write', function () {
+      expect(acl.isAllowedTo("presentation", "read", mockCredentials)).to.be(true);
+      expect(acl.isAllowedTo("presentation", "write", mockCredentials)).to.be(true);
+    });
+  });
+});
+
+const UsersViewer = {};
+
+UsersViewer.findOne = function () {
+  return {
+    role: "viewer"
+  };
+}
+
+const UsersModerator = {};
+
+UsersModerator.findOne = function () {
+  return {
+    role: "moderator"
+  };
+}
+
+const aclConfig = {
+  "viewer": {
+    "chatPublic": {
+      "write": true,
+      "read": true
+    },
+    "presentation": {
+      "write": false,
+      "read": true
+    }
+  },
+  "moderator": {
+    "chatPublic": {
+      "write": true,
+      "read": true
+    },
+    "chatPrivate": {
+      "write": true,
+      "read": true
+    },
+    "presentation": {
+      "write": true,
+      "read": false
+    },
+    "lockSetting": {
+      "write": true,
+      "read": false
+    }
+  }
+};
+
+const mockCredentials = {
+  meetingId: "meetingId",
+  requesterUserId: "requesterUserId",
+  requesterToken: "requesterToken"
+};
